8612c4dabb97365fde8be9089ef416d236a8020f
Dave Liggat <dliggat@gmail.com>
2014-02-15 14:55:56 -0500

    Adding airport IP

--- modules/people/manifests/dliggat/hosts.pp
+++ modules/people/manifests/dliggat/hosts.pp
@@ -3,11 +3,14 @@ class people::dliggat::hosts {
   notify { 'class people::dliggat::hosts declared': }
 
   # For local network DHCP reservations.
-  host { "tilde":
-    ip => "10.0.1.3",
+  host { "airport":
+    ip => "10.0.1.1",
   }
   host { "mite":
     ip => "10.0.1.2",
+  }
+  host { "tilde":
+    ip => "10.0.1.3",
   }
   host { "boggle":
     ip => "10.0.1.4",
